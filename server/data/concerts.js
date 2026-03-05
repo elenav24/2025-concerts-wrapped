b0d4fe45492b64a8e799700591760e26d0c0d70f
elena marquez <elena-marquez@csu.fullerton.edu>
2026-03-04 16:43:55 -0800

    finished assignment

--- server/data/concerts.js
+++ server/data/concerts.js
@@ -1,56 +1,64 @@
 const concertData = [
     {
+        id: 1,
         artist: "Tate Mcrae",
-        date: "2025-02-20",
+        date: "02-20-2025",
         venue: "Kia Forum parking lot",
         title: "Album Release Event",
         imageURL: "https://s.yimg.com/ny/api/res/1.2/FIRChsqgrucJRvv8.r458g--/YXBwaWQ9aGlnaGxhbmRlcjt3PTE4MDA7aD0xMjcyO2NmPXdlYnA-/https://media.zenfs.com/en/ktla_articles_362/9e928ac51dcfc804c68fd54f2a6248ae"
     },
     {
+        id: 2,
         artist: "The Driver Era",
-        date: "2025-07-09",
+        date: "07-09-2025",
         venue: "WAMU Theater",
         title: "Obsession Tour",
         imageURL:"https://seatgeekimages.com/performers-landscape/the-driver-era-f78199/700265/57012/720x450.jpg"
     },
     {
+        id: 3,
         artist: "Laufey",
-        date: "2025-09-26",
+        date: "09-26-2025",
         venue: "Crypto.com Arena",
         title: "A Matter of Time Tour",
         imageURL: "https://www.hollywoodreporter.com/wp-content/uploads/2025/09/20250926-laufey-losangeles-nmago-4311-e1759084372954.jpg?w=1500&h=845&crop=1"
     },
     {
+        id: 4,
         artist: "Chappell Roan",
-        date: "2025-10-11",
+        date: "10-11-2025",
         venue: "Brookside at the Rose Bowl",
-        title: "Visions of Damsels & Other Dangerous Things Pop-Up Show",
+        title: "Visions of Damsels & Other Dangerous Things",
         imageURL: "https://www.billboard.com/wp-content/uploads/2025/10/01-chappel-roan-rose-bowl-2025-billboard-1800.jpg?w=875&h=583&crop=1"
     },
     {
+        id: 5,
         artist: "Blood Orange",
-        date: "2025-10-16",
+        date: "10-16-2025",
         venue: "Shrine Expo Hall",
         title: "Essex Honey Tour",
         imageURL: "https://uproxx.com/wp-content/uploads/2022/09/blood-orange-grid.jpg?w=710"
     },
     {
+        id: 6,
         artist: "Renee Rapp",
-        date: "2025-10-17",
+        date: "10-17-2025",
         venue: "Kia Forum",
         title: "Bite Me Tour",
         imageURL: "https://www.billboard.com/wp-content/uploads/2025/09/renee-rapp-bite-me-tour-opening-night-10-2025-billboard-1800.jpg?w=942&h=628&crop=1"
     },
     {
+        id: 7,
         artist: "Tate Mcrae",
-        date: "2025-11-07",
+        date: "11-07-2025",
         venue: "Acrisure Arena",
         title: "Miss Possessive Tour",
         imageURL: "https://assets.shazam.com/events/photoalbum/1500x1500/a458f15f-05e0-4e6a-b746-e2ab659512fd.jpg"
     },
     {
+        id: 8,
         artist: "Sabrina Carpenter",
-        date: "2025-11-20",
+        date: "11-20-2025",
         venue: "Crypto.com Arena",
         title: "Short n' Sweet Tour",
         imageURL: "https://assets.teenvogue.com/photos/6706c8cd8ba431e2b0123cb6/master/w_1600%2Cc_limit/GettyImages-2175962908.jpg"
---
+++ server/public/scripts/concert.js
@@ -0,0 +1,28 @@
+const renderConcert = async () => {
+    const requestedID = parseInt(window.location.href.split('/').pop())
+
+    const response = await fetch('/concerts')
+    const data = await response.json()
+
+    const concertContent = document.getElementById('concert-content')
+    let concert
+
+    concert = data.find(concert => concert.id === requestedID)
+
+    if (concert) {
+        document.getElementById('concert-detail-image').src = concert.imageURL
+        document.getElementById('concert-artist').textContent = concert.artist
+        document.getElementById('concert-title').textContent = concert.title
+        document.getElementById('concert-venue').textContent = `Venue: ${concert.venue}`
+        document.getElementById('concert-date').textContent = `Date: ${concert.date}`
+
+        document.title = `${concert.artist} - ${concert.title}`
+    }
+    else {
+        const errorMessage = document.createElement('p')
+        errorMessage.textContent = 'No concert data available :('
+        concertContent.appendChild(errorMessage)
+    }
+}
+
+renderConcert()
